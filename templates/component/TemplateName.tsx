68eaee592d53816ac7cd8e410bda4ecc18923c77
dfx_17 <rossohinpavel@yandex.ru>
2026-06-06 12:02:58 +0300

    simple component generator

---
+++ templates/component/TemplateName.tsx
@@ -0,0 +1,14 @@
+import styles from './TemplateName.module.css';
+
+
+export interface TemplateNameProps {}
+
+export function TemplateName(props: TemplateNameProps) {
+  const { } = props;
+
+  return (
+    <div className={styles.MyComponent}>
+      MyComponent Component
+    </div>
+  );
+}
